1c176a568285fb8e860b33dd3d8505f28829f48e
Danson <githukadanson23@gmail.com>
2025-06-13 01:01:53 +0300

    first commit

---
+++ script.js
@@ -0,0 +1,17 @@
+ new Typed('#typed-name', {
+    strings: [ "Hi"," I am Danson Githuka", "Frontend Developer", "Problem Solver", 'Tech Enthusiast,' ,'i welcome you to view my portfolio', "let's connect!"],
+    typeSpeed: 70,
+    backSpeed: 40,
+    backDelay: 1200,
+    startDelay: 400,
+    loop: true
+  });
+
+  new Typed('#typed-desc', {
+    strings: ["Get in touch with me for any inquiries, collaborations, or just to say hello!"],
+    typeSpeed: 60,
+    backSpeed: 30,
+    backDelay: 1500,
+    startDelay: 500,
+    loop: true
+  });
